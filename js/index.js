6cbb63812c5b64de6ec68dfc51b38886dc1c1b7e
lyntechi <lynda-santiago@lambdastudents.com>
2020-06-03 01:53:44 -0400

    created one event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+
+let header = document.querySelector("header")
+header.onmouseover = mouseOver;
+header.onmouseout = mouseOut;
+
+function mouseOver() {
+header.style.backgroundColor = "yellow"; 
+}
+
+function mouseOut() {
+    header.style.backgroundColor = "white"; 
+    }
+    
+
+    // header.addEventListener("onmouseover", function(event){
+    //     event.target.style.backgroundColor = "yellow";
+    // })
+
+    // header.addEventListener("onmouseout", function(event) {
+    //     event.target.backgroundColor= "white";
+       
+    // })
+
+document.addEventListener('keydown', alertPopsOut);
+
+function alertPopsOut (event) {
+  if (event.target === 'keydown'){
+      alert('hi how are you')
+  };
+}
